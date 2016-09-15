9d6b7b41b42d7f195cd070b0bcd765223ac460a6
dreamanlan <dreaman_163@163.com>
2016-09-16 00:04:34 +0800

    操作符优先级问题修改

--- Common/SimpleScript/SlkToken.h
+++ Common/SimpleScript/SlkToken.h
@@ -2,7 +2,7 @@
 
     SlkToken.h
 
-**************************************************************************/
+    **************************************************************************/
 
 #ifndef _SLK_SLKTOKEN_H
 #define _SLK_SLKTOKEN_H
@@ -12,64 +12,70 @@
 class SlkToken
 {
 public:
-	short get(void);
-	short peek(int level);
-	char* getCurToken(void) const
-	{
-		return mCurToken;
-	}
-	char* getLastToken(void) const
-	{
-		return mLastToken;
-	}
-	int getLineNumber(void) const
-	{
-		return mLineNumber;
-	}
-	int getLastLineNumber(void) const
-	{
-		return mLastLineNumber;
-	}
+  short get(void);
+  short peek(int level);
+  char* getCurToken(void) const
+  {
+    return mCurToken;
+  }
+  char* getLastToken(void) const
+  {
+    return mLastToken;
+  }
+  int getLineNumber(void) const
+  {
+    return mLineNumber;
+  }
+  int getLastLineNumber(void) const
+  {
+    return mLastLineNumber;
+  }
 public:
-	void setCanFinish(int val)
-	{
-		mCanFinish=val;
-	}
+  void setCanFinish(int val)
+  {
+    mCanFinish = val;
+  }
 private:
-	void getOperatorToken(void);
-	short getOperatorTokenValue(void)const;
-	int isCanFinish(void)const
-	{
-		return mCanFinish;
-	}
-	int isWhiteSpace(char c) const;
-	int isDelimiter(char c) const;
-	int isOperator(char c) const;
+  void getOperatorToken(void);
+  short getOperatorTokenValue(void)const;
+  int isCanFinish(void)const
+  {
+    return mCanFinish;
+  }
+  int isWhiteSpace(char c) const;
+  int isDelimiter(char c) const;
+  int isBeginParentheses(char c) const;
+  int isEndParentheses(char c) const;
+  int isOperator(char c) const;
+  int isSpecialChar(char c) const;
 private:
-	void newToken(void);
-	void pushTokenChar(char c);
-	void endToken(void);
-	void endTokenWithEof(void);
+  void newToken(void);
+  void pushTokenChar(char c);
+  void endToken(void);
+  void endTokenWithEof(void);
 public:
-	SlkToken(FunctionScript::IScriptSource& source,FunctionScript::ErrorAndStringBuffer& errorAndStringBuffer);
+  SlkToken(FunctionScript::IScriptSource& source, FunctionScript::ErrorAndStringBuffer& errorAndStringBuffer);
 private:
-	FunctionScript::IScriptSource* mSource;
-	FunctionScript::IScriptSource::Iterator mIterator;
+  FunctionScript::IScriptSource* mSource;
+  FunctionScript::IScriptSource::Iterator mIterator;
 
-	char* mCurToken;
-	char* mLastToken;
-	int mTokenCharIndex;
+  char* mCurToken;
+  char* mLastToken;
+  int mTokenCharIndex;
 
-	FunctionScript::ErrorAndStringBuffer* mErrorAndStringBuffer;
+  FunctionScript::ErrorAndStringBuffer* mErrorAndStringBuffer;
 
-	int mLineNumber;
-	int mLastLineNumber;
-	int mIsExternScript;
-	int mCanFinish;
+  int mLineNumber;
+  int mLastLineNumber;
+  int mIsExternScript;
+  int mCanFinish;
 
-	const char* mWhiteSpaces;
-	const char* mDelimiters;
-	const char* mOperators;
+  const char* mWhiteSpaces;
+  const char* mDelimiters;
+  const char* mBeginParentheses;
+  const char* mEndParentheses;
+  const char* mOperators;
+  const char* mSpecialChars;
 };
 
 #endif
