3d64fa49dc42bde920da96bfa0dba9238b76f173
dreamanlan <dreaman_163@163.com>
2022-05-08 14:18:08 +0800

    词法解析添加对unicode字符串转义支持

--- Common/SimpleScript/SlkToken.h
+++ Common/SimpleScript/SlkToken.h
@@ -8,115 +8,131 @@
 #define _SLK_SLKTOKEN_H
 
 #include "calc.h"
+#include "Queue.h"
 
 class SlkToken
 {
 public:
-	short get(void);
-	short peek(int level);
-	char* getCurToken(void) const
-	{
-		return mCurToken;
-	}
-	char* getLastToken(void) const
-	{
-		return mLastToken;
-	}
-	int getLineNumber(void) const
-	{
-		return mLineNumber;
-	}
-	int getLastLineNumber(void) const
-	{
-		return mLastLineNumber;
-	}
-	int getCommentNum(int& commentOnNewLine) const
-	{
-		commentOnNewLine = mCommentOnNewLine;
-		return mCommentNum;
-	}
-	char* getComment(int index) const
-	{
-		if (index >= 0 && index < mCommentNum) {
-			return mComments[index];
-		}
-		else {
-			return 0;
-		}
-	}
-	void resetComments(void)
-	{
-		mCommentNum = 0;
-		mCommentOnNewLine = FALSE;
-	}
+    short get(void);
+    short peek(int level);
+    char* getCurToken(void) const
+    {
+        return mCurToken;
+    }
+    char* getLastToken(void) const
+    {
+        return mLastToken;
+    }
+    int getLineNumber(void) const
+    {
+        return mLineNumber;
+    }
+    int getLastLineNumber(void) const
+    {
+        return mLastLineNumber;
+    }
+    int getCommentNum(int& commentOnNewLine) const
+    {
+        commentOnNewLine = mCommentOnNewLine;
+        return mCommentNum;
+    }
+    char* getComment(int index) const
+    {
+        if (index >= 0 && index < mCommentNum) {
+            return mComments[index];
+        }
+        else {
+            return 0;
+        }
+    }
+    void resetComments(void)
+    {
+        mCommentNum = 0;
+        mCommentOnNewLine = FALSE;
+    }
 public:
-	void setCanFinish(int val)
-	{
-		mCanFinish = val;
-	}
-	void setStringDelimiter(const char* begin, const char* end);
-	void setScriptDelimiter(const char* begin, const char* end);
+    void setCanFinish(int val)
+    {
+        mCanFinish = val;
+    }
+    void setStringDelimiter(const char* begin, const char* end);
+    void setScriptDelimiter(const char* begin, const char* end);
 private:
-	void getOperatorToken(void);
-	short getOperatorTokenValue(void)const;
-	int isCanFinish(void)const
-	{
-		return mCanFinish;
-	}
-	int isWhiteSpace(char c) const;
-	int isDelimiter(char c) const;
-	int isBeginParentheses(char c) const;
-	int isEndParentheses(char c) const;
-	int isOperator(char c) const;
+    char curChar(void)const;
+    char nextChar(void)const;
+    char peekChar(int ix)const;
+    void getOperatorToken(void);
+    short getOperatorTokenValue(void)const;
+    int isCanFinish(void)const
+    {
+        return mCanFinish;
+    }
+    int isWhiteSpace(char c) const;
+    int isDelimiter(char c) const;
+    int isBeginParentheses(char c) const;
+    int isEndParentheses(char c) const;
+    int isOperator(char c) const;
     int isQuote(char c) const;
-	int isSpecialChar(char c) const;
+    int isSpecialChar(char c) const;
 private:
     int isBegin(const char* delimiter, int len) const;
-	void getBlockString(const char* delimiter, int len);
-	void removeFirstAndLastEmptyLine(void);
-	void newComment(void);
-	void pushCommentChar(char c);
-	void endComment(void);
-	void newToken(void);
-	void pushTokenChar(char c);
-	void endToken(void);
-	void endTokenWithEof(void);
+    void getBlockString(const char* delimiter, int len);
+    void removeFirstAndLastEmptyLine(void);
+    void newComment(void);
+    void pushCommentChar(char c);
+    void endComment(void);
+    void newToken(void);
+    void pushTokenChar(char c);
+    void tempEndToken(void);
+    void endToken(void);
+    void endTokenWithEof(void);
 public:
 	SlkToken(FunctionScript::IScriptSource& source, FunctionScript::ErrorAndStringBuffer& errorAndStringBuffer);
 private:
+    struct TokenInfo
+    {
+        char* Token;
+        short TokenValue;
+        int LineNumber;
+
+        inline TokenInfo(void) {}
+        inline TokenInfo(char* token, short tokenValue, int lineNumber) :Token(token), TokenValue(tokenValue), LineNumber(lineNumber) {}
+    };
+
     FunctionScript::IScriptSource* mSource;
     FunctionScript::IScriptSource::Iterator mIterator;
 
-	char* mCurToken;
-	char* mLastToken;
-	int mTokenCharIndex;
+    char* mCurToken;
+    char* mLastToken;
+    int mTokenCharIndex;
 
-	char* mCurComment;
-	int mCommentCharIndex;
+    char* mCurComment;
+    int mCommentCharIndex;
 
-	static const int c_MaxCommentNum = 256;
-	char* mComments[c_MaxCommentNum];
-	int mCommentNum;
-	int mCommentOnNewLine;
+    static const int c_MaxCommentNum = 256;
+    char* mComments[c_MaxCommentNum];
+    int mCommentNum;
+    int mCommentOnNewLine;
     FunctionScript::ErrorAndStringBuffer* mErrorAndStringBuffer;
+    DequeT<TokenInfo, 16> mTokenQueue;
 
-	int mLineNumber;
-	int mLastLineNumber;
-	int mCanFinish;
+    int mLineNumber;
+    int mLastLineNumber;
+    int mCanFinish;
 
-	const char* mWhiteSpaces;
-	const char* mDelimiters;
-	const char* mBeginParentheses;
-	const char* mEndParentheses;
-	const char* mOperators;
+    const char* mWhiteSpaces;
+    const char* mDelimiters;
+    const char* mBeginParentheses;
+    const char* mEndParentheses;
+    const char* mOperators;
     const char* mQuotes;
-	const char* mSpecialChars;
+    const char* mSpecialChars;
 
-	static const int c_MaxDelimiterSize = 1024;
-	char mScriptBeginDelimiter[c_MaxDelimiterSize + 1];
-	char mScriptEndDelimiter[c_MaxDelimiterSize + 1];
-	char mStringBeginDelimiter[c_MaxDelimiterSize + 1];
-	char mStringEndDelimiter[c_MaxDelimiterSize + 1];
+    static const int c_MaxDelimiterSize = 1024;
+    char mScriptBeginDelimiter[c_MaxDelimiterSize + 1];
+    char mScriptEndDelimiter[c_MaxDelimiterSize + 1];
+    char mStringBeginDelimiter[c_MaxDelimiterSize + 1];
+    char mStringEndDelimiter[c_MaxDelimiterSize + 1];
     int mScriptBeginDelimiterLength = 0;
     int mScriptEndDelimiterLength = 0;
     int mStringBeginDelimiterLength = 0;
